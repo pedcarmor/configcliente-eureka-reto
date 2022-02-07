bfaa0179c2247b6cb97b44e5044db1f3204b67f3
pedcarmor <pcarvajm@emeal.nttdata.com>
2022-02-07 14:52:53 +0100

    Primer microservicio

---
+++ src/main/java/com/nttdata/configclient/eureka/ClientEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.configclient.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ClientEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/configclient/eureka/ClientEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.configclient.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
