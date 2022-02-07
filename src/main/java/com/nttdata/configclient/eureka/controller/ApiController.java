c5c5ff881d58c3a57dd77a9e979f1af08060703d
pedcarmor <pcarvajm@emeal.nttdata.com>
2022-02-07 14:54:39 +0100

    myValue

--- src/main/java/com/nttdata/configclient/eureka/controller/ApiController.java
+++ src/main/java/com/nttdata/configclient/eureka/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.nttdata.configclient.eureka.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiController {
 
+	@Value("${some.value}")
+	private String myValue;
+	
+	@GetMapping(path="/myvalue")
+	public String myValue() {
+		return this.myValue;
+	}
+	
 	@GetMapping("/suma/{a}/{b}")
 	public Integer suma(@PathVariable final Integer a,@PathVariable final Integer b) {
 		return a + b;
